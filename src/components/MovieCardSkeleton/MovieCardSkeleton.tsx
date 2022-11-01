2bb00cdd20131bb6b3238443e3b3dbc7cb58ec62
Adnan Ebrahimi <adnane.ebrahimi@gmail.com>
2022-11-01 14:00:24 +0330

    fix padding in skeleton

--- src/components/MovieCardSkeleton/MovieCardSkeleton.tsx
+++ src/components/MovieCardSkeleton/MovieCardSkeleton.tsx
@@ -3,7 +3,7 @@ import Skeleton from "@mui/material/Skeleton";
 
 export default function MovieCardSkeleton() {
   return (
-    <Paper className="flex flex-col gap-2 h-64">
+    <Paper className="flex flex-col gap-2 h-64 p-3">
       <Skeleton variant="rectangular" width={100} height={150} />
       <Skeleton variant="rounded" width={210} height={60} />
     </Paper>
